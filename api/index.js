b1c018933df67f56c2f2e88e2c52b150e48760c7
ghopkins3 <ghopkins3@murraystate.edu>
2025-03-28 15:45:54 -0500

    Update index

--- api/index.js
+++ api/index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.get("/test", (req, res) => {
+    res.send("this works!");
+});
+
 app.listen(PORT, (error) => {
     if(!error) {
         console.log(`Server is running on http://localhost:${PORT}`)
